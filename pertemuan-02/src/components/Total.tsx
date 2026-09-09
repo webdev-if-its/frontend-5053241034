9e89e8a66da09a0d0421453745d6e3382425a0df
KajeQ <zakialfikri313@gmail.com>
2026-09-09 17:51:03 +0700

    pertemuan-02: level 3 selesai

--- pertemuan-02/src/components/Total.tsx
+++ pertemuan-02/src/components/Total.tsx
@@ -2,6 +2,14 @@
 // Lalu render satu elemen yang memuat teks "Total: " diikuti HASIL PERKALIAN
 // qty * harga (pakai ekspresi { } di JSX, jangan hardcode angkanya). Lihat
 // SOAL.md untuk kontrak lengkap.
-export function Total(props: any) {
-  return <p>TODO</p>
+
+type Props = {
+  qty: number
+  harga: number
+}
+
+export function Total(props: Props) {
+  return (
+    <p>Total: {props.qty * props.harga}</p>
+  )
 }
