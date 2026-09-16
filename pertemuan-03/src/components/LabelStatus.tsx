2180731f0eb0f8a0841428f26821aa0871f1a80d
KajeQ <zakialfikri313@gmail.com>
2026-09-16 17:14:10 +0700

    pertemuan-03: level 4 selesai

--- pertemuan-03/src/components/LabelStatus.tsx
+++ pertemuan-03/src/components/LabelStatus.tsx
@@ -2,6 +2,11 @@
 // TERNARY OPERATOR ( ? : ) di dalam JSX untuk menampilkan teks "Selesai"
 // saat selesai bernilai true, atau "Belum Selesai" saat false. Lihat
 // SOAL.md untuk kontrak lengkap.
-export function LabelStatus(props: any) {
-  return <span>TODO</span>
+
+type Props = {
+  selesai: boolean
+}
+
+export function LabelStatus(props: Props) {
+  return <p>{props.selesai ? "Selesai" : "Belum Selesai"}</p>
 }
